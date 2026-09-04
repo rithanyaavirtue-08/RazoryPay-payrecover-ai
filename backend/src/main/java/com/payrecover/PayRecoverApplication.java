5b40b3498ee43bfffb6d5da03c5503c33601f140
Rithanyaa B. <rithanyaa.virtue@gmail.com>
2026-09-04 17:03:27 +0530

    Initialize

---
+++ backend/src/main/java/com/payrecover/PayRecoverApplication.java
@@ -0,0 +1,11 @@
+package com.payrecover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayRecoverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PayRecoverApplication.class, args);
+	}
+}
